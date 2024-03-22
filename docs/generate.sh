6556726f3e0971e2b095baf236ca72661c9febb0
Egor Seredin <egor@power.trade>
2024-03-22 03:41:29 +0000

    Docs: move to api-docs-source

--- docs/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-asciidoctor-pdf order_entry/root.asciidoc -o OrderEntry.pdf
-asciidoctor-pdf drop_copy/root.asciidoc -o DropCopy.pdf
-
-asciidoctor -v order_entry/root.asciidoc -o OrderEntry.html
-asciidoctor -v drop_copy/root.asciidoc -o DropCopy.html
